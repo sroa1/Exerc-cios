593f6d0201702122dd029c78686e811eb6f93008
aluno <aluno@LAB0X-P0X.labs.riolargo>
2019-12-16 09:00:58 -0300

    Criacao de metodos get para todos os atributos

--- src/modelo/Time.java
+++ src/modelo/Time.java
@@ -1,10 +1,31 @@
 package modelo;
 
 public class Time{
-    
+
     private String nome;
     private String categoria;
     private int anoDeCriacao;
     private int quantidadeDeSocios;
     private String historico;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getAnoDeCriacao() {
+        return anoDeCriacao;
+    }
+
+    public int getQuantidadeDeSocios() {
+        return quantidadeDeSocios;
+    }
+
+    public String getHistorico() {
+        return historico;
+    }
+
 }
