82e9e4a0f4c440d275c5240bd15057a5b15c32d4
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2019-12-16 09:16:51 -0300

    Construtor e métodos set

--- src/modelo/Time.java
+++ src/modelo/Time.java
@@ -1,10 +1,31 @@
 package modelo;
 
-public class Time{
-    
+public class Time {
+
     private String nome;
     private String categoria;
     private int anoDeCriacao;
     private int quantidadeDeSocios;
     private String historico;
+
+    public Time(String nome, String categoria) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.anoDeCriacao = 0;
+        this.quantidadeDeSocios = 0;
+        this.historico = "indefinido";
+    }
+
+    public void setAnoDeCriacao(int anoDeCriacao) {
+        this.anoDeCriacao = anoDeCriacao;
+    }
+
+    public void setQuantidadeDeSocios(int quantidadeDeSocios) {
+        this.quantidadeDeSocios = quantidadeDeSocios;
+    }
+
+    public void setHistorico(String historico) {
+        this.historico = historico;
+    }
+
 }
