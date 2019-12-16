d2aa4f6f0851c07cd10470469e86721f5bbd9c31
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2019-12-16 11:05:01 -0300

    classe Torcedor com construtor

---
+++ src/modelo/Torcedor.java
@@ -0,0 +1,12 @@
+public class Torcedor {
+    private String nome;
+    private String sexo;
+    private int idade;
+
+    public Torcedor(String nome, String sexo, int idade) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+    }
+
+}
