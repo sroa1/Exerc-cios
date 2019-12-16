50f9f684ab10ca13c29b2b8e1ae015b281cd271a
aluno <aluno@LAB0X-P0X.labs.riolargo>
2019-12-16 08:51:05 -0300

    Criacao do projeto

---
+++ src/modelo/Time.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class Time{
+    
+    private String nome;
+    private String categoria;
+    private int anoDeCriacao;
+    private int quantidadeDeSocios;
+    private String historico;
+}
